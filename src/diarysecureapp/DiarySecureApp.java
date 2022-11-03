f0be2bc8e816477a5af6c600f96f8f5b3258da23
FabianGal45 <fabiangal.45@gmail.com>
2022-11-03 13:25:22 +0000

    First commit

---
+++ src/diarysecureapp/DiarySecureApp.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package diarysecureapp;
+
+/**
+ *
+ * @author galfa
+ */
+public class DiarySecureApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //TESTTTTTTTT
+    }
+    
+}
