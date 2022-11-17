9ad9fd1ddb089f6825aadd8120bd4310e90bfd84
tazmu <tazmu@DESKTOP-3BBA8D5>
2022-11-17 19:33:42 +0000

    Merge origin/master
    Conflicts:
    	src/diarysecureapp/DiarySecureApp.java

--- src/diarysecureapp/SHA.java
+++ src/diarysecureapp/SHA.java
@@ -41,11 +41,11 @@ public class SHA {
     private static String bytesToHex(byte[] hash) {
         StringBuilder hexString = new StringBuilder(2 * hash.length);
         for (int i = 0; i < hash.length; i++) {
-            String hex = Integer.toHexString(0xff & hash[i]);
-            if (hex.length() == 1) {
+            String hexDigit = Integer.toHexString(0xff & hash[i]);
+            if (hexDigit.length() == 1) {
                 hexString.append('0');
             }
-            hexString.append(hex);
+            hexString.append(hexDigit);
         }
         return hexString.toString();
     }
